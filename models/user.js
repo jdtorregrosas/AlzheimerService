0b2f0a072c88bd3014cb910efd8d02942d4ddb6e
Julián David <jdtorregrosas@gmail.com>
2017-05-20 18:35:05 -0500

    Change db structure

--- models/user.js
+++ models/user.js
@@ -1,12 +1,20 @@
 const Joi = require('joi');
 
 const User = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
+  Profile: {
+    address: Joi.string(),
+    birthdate: Joi.string(),
+    cellphone: Joi.string(),
+    firstname: Joi.string(),
+    gender: Joi.string(),
+    lastname: Joi.string(),
+    maritalStatus: Joi.string(),
+    telephone: Joi.string()
+  },
+  Auth: {
+    email: Joi.string().email(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
-    names: Joi.string().required(),
-    lastnames: Joi.string().required(),
-    birthyear: Joi.number().integer().min(1900).max(2013),
-    email: Joi.string().email()
+  }
 });
 
 const UserWOCredentials = Joi.object().keys({
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app        = express();
 var bodyParser = require('body-parser');
 var User = require('./models/user');
 var firebase = require("firebase-admin");
-var serviceAccount = require("./brein-78ed7-firebase-adminsdk-32ruk-c0cfce67fe.json");
+var serviceAccount = require("./key.json");
 var validateUser = require('./models/user').validateUser;
 var validateUserWOCredentials = require('./models/user').validateUserWOCredentials;
 var validateAuthUser = require('./models/auth').validateAuthUser;
@@ -15,11 +15,11 @@ const Joi = require('joi');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://brein-78ed7.firebaseio.com"
+  databaseURL: "https://tpi-alzheimer-android.firebaseio.com/"
 });
 
 var db = firebase.database();
-var ref = db.ref("alzheimer_db");
+var ref = db.ref("/");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -36,8 +36,8 @@ var router = express.Router();
 // Users: get all
 router.route('/users')
 .get(function(req, res) {
-    var usersRef = ref.child("users");
-    usersRef.once("value", function(snapshot) {
+    ref.once("value", function(snapshot) {
+      console.log(snapshot);
       res.json(snapshot.val());
     }, function (errorObject) {
       res.json({error: errorObject});
@@ -45,36 +45,65 @@ router.route('/users')
 })
 .post(function(req, res) { // Users: create new user
   validateUser(req.body).then((user) => {
-    var userRef = ref.child("users/"+user.username);
-    userRef.once("value", function(obj) {
-      console.log(obj.val());
-      if(obj.val() == null){
-        var users = ref.child("users");
-        user.password = new Buffer(user.password).toString('base64');
-        users.child(user.username).set(user);
-        res.json(user);
-      } else {
-        res.status(409).json({error: 'User already exists'});
-      }
+    firebase.auth().createUser({
+      email: user.Auth.email,
+      emailVerified: false,
+      password: user.Auth.password,
+      displayName: user.Profile.firstname + ' ' + user.Profile.lastname,
+      disabled: false
+    })
+    .then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+
+        var userRef = ref.child(userRecord.uid);
+        userRef.once("value", function(obj) {
+          if(obj.val() == null){
+            user.Auth.password = new Buffer(user.Auth.password).toString('base64');
+            ref.child(userRecord.uid).set(user);
+            res.json(user);
+          } else {
+            res.status(409).json({error: 'User already exists'});
+          }
+        });
+
+    })
+    .catch(function(error) {
+      res.status(400).json({error});
+      console.log("Error creating new user:", error);
     });
   }).catch((err) => {
     res.status(400).json({error: err});
   });
 });
 // Users: get one user
-router.route('/users/:username')
+router.route('/users/:email')
 .get(function(req, res) {
-  var username = req.params.username;
-  var usersRef = ref.child("users/"+username);
-  usersRef.once("value", function(obj) {
-    if(obj.val() == null){
-      res.status(404).json({error: 'User not found'});
-    } else {
-      res.json(obj.val());
-    }
-  }, function (err) {
-    res.status(404).json(err);
+  var email = req.params.email;
+  firebase.auth().getUserByEmail(email)
+  .then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    var userRef = ref.child(userRecord.uid);
+    userRef.once("value", function(snapshot) {
+      console.log(snapshot);
+      res.json(snapshot.val());
+    }, function (errorObject) {
+      res.json({error: errorObject});
+    });
+  })
+  .catch(function(error) {
+    res.json({error: errorObject});
   });
+  // var username = req.params.username;
+  // var usersRef = ref.child("users/"+username);
+  // usersRef.once("value", function(obj) {
+  //   if(obj.val() == null){
+  //     res.status(404).json({error: 'User not found'});
+  //   } else {
+  //     res.json(obj.val());
+  //   }
+  // }, function (err) {
+  //   res.status(404).json(err);
+  // });
 }).put(function(req, res) { // Users: edit user
   var username = req.params.username;
   validateUserWOCredentials(req.body).then((user) => {
